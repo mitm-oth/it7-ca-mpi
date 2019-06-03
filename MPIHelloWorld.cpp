f878c1a3fce76abd0090f70fb8057121c3274ed0
Jonathan Weber <bejonwe@gmx.net>
2019-06-04 01:11:04 +0200

    Make it Intel C++ compiler compatible

--- MPIHelloWorld.cpp
+++ MPIHelloWorld.cpp
@@ -13,12 +13,12 @@ int main(int argc, char **argv)
     if (rank == 0)
     {
         char helloStr[] = "Hello World";
-        MPI_Send(helloStr, _countof(helloStr), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(helloStr, strlen(helloStr), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
     }
     else if (rank == 1)
     {
         char helloStr[12];
-        MPI_Recv(helloStr, _countof(helloStr), MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(helloStr, strlen(helloStr), MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Rank 1 received string %s from Rank 0\n", helloStr);
     }
 
